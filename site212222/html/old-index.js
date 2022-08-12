9fd0fbb7835c454bc5c12dfe590c5406e4b7627f
Alex <alexandru.nicolescu06@gmail.com>
2022-08-12 13:49:33 +0200

    first index

---
+++ site212222/html/old-index.js
@@ -0,0 +1,149 @@
+﻿/*
+File: index.js
+Author: Fabio Vitali
+Version: 1.0 
+Last change on: 10 April 2021
+
+
+Copyright (c) 2021 by Fabio Vitali
+
+   Permission to use, copy, modify, and/or distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
+
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+   SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+   OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+   CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+*/
+
+/* ========================== */
+/*                            */
+/*           SETUP            */
+/*                            */
+/* ========================== */
+
+
+global.rootDir = __dirname ;
+global.startDate = null; 
+
+const template = require(global.rootDir + '/scripts/tpl.js'); 
+const connectDB = require(global.rootDir + '/scripts/mongo-db.js'); 
+const express = require('express');
+const { engine } = require('express-handlebars');
+const cors = require('cors');
+const path = require('path');
+
+let app = express(); 
+
+// Handlebars
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+/* ========================== */
+/*                            */
+/*           MONGODB          */
+/*                            */
+/* ========================== */
+
+const mongoCredentials = {
+	user: "site212222",
+	pwd: "Ziu0reeh",
+	site: "mongo_site212222"
+}
+/* end */
+
+connectDB.init(mongoCredentials);
+
+
+/* ========================== */
+/*                            */
+/*  EXPRESS CONFIG & ROUTES   */
+/*                            */
+/* ========================== */
+
+app.use('/js'  , express.static(global.rootDir +'/public/js'));
+app.use('/css' , express.static(global.rootDir +'/public/css'));
+app.use('/data', express.static(global.rootDir +'/public/data'));
+app.use('/docs', express.static(global.rootDir +'/public/html'));
+app.use('/img' , express.static(global.rootDir +'/public/media'));
+app.use(express.urlencoded({ extended: true })) 
+app.use(cors())
+
+// https://stackoverflow.com/questions/40459511/in-express-js-req-protocol-is-not-picking-up-https-for-my-secure-link-it-alwa
+app.enable('trust proxy');
+
+
+// Routes
+app.use('/', require('./routes/index'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', async function (req, res) {
+	res.sendFile(path.join(__dirname+'/login/login.html'));
+})
+
+
+/*app.get('/', async function (req, res) { 
+	res.sendFile(path.join(__dirname+'/login/login.html'));
+})*/
+
+app.get('/hw', async function(req, res) { 
+	var text = "Hello world as a Node service";
+	res.send(
+`<!doctype html>
+<html>
+	<body>
+		<h1>${text}</h1>
+		<p><a href="javascript:history.back()">Go back</a></p>
+	</body>
+</html>
+			`)
+});
+
+app.get('/hwhb', async function(req, res) { 
+	res.send(await template.generate('generic.html', {
+		text: "Hello world as a Handlebar service",
+	}));
+});
+
+const info = async function(req, res) {
+	let data = {
+		startDate: global.startDate.toLocaleString(), 
+		requestDate: (new Date()).toLocaleString(), 
+		request: {
+			host: req.hostname,
+			method: req.method,
+			path: req.path,
+			protocol: req.protocol
+		}, 
+		query: req.query,
+		body: req.body
+	}
+	res.send( await template.generate('info.html', data));
+}
+
+app.get('/info', info )
+app.post('/info', info )
+
+
+/* ========================== */
+/*                            */
+/*    ACTIVATE NODE SERVER    */
+/*                            */
+/* ========================== */
+
+app.listen(8000, function() { 
+	global.startDate = new Date() ; 
+	console.log(`App listening on port 8000 started ${global.startDate.toLocaleString()}` )
+})
+
+
+/*       END OF SCRIPT        */
