aabc6811455d217574b51e074e8888480d2df574
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-13 23:46:03 +0100

    turned autocomplete off

--- frontoffice/src/components/Auth/RegisterCard.js
+++ frontoffice/src/components/Auth/RegisterCard.js
@@ -28,7 +28,7 @@ function RegisterCard(){
     { !isLogged &&
     <div className="flex items-center flex-col">
         <div className="prose"> <h1> Crea Account </h1> </div>
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96" onSubmit={handleSubmit(onSubmit)}>
+        <form autoComplete="off" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96" onSubmit={handleSubmit(onSubmit)}>
 
             <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
