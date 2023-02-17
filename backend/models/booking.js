5330fcaac9e5020e173468ad8b4958a02c54b923
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-17 15:11:48 +0100

    modified service and booking schema & added default image

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -11,21 +11,13 @@ const BookingSchema = new mongoose.Schema({
     ref: "User",
     required: [true, "Serve l'ID dell'utente che prenota il servizio"],
   },
-  date: {
+  startDate: {
     type: Date,
     required: [true, "Serve la data di prenotazione"],
   },
-  startHour: {
-    type: Number,
-    min: 9,
-    max: 16,
-    required: [true, "Serve l'ora d'inizio della prenotazione"],
-  },
-  endHour: {
-    type: Number,
-    min: 10,
-    max: 17,
-    required: [true, "Serve l'ora di fine della prenotazione"],
+  endDate: {
+    type: Date,
+    required: [true, "Serve la data di prenotazione"],
   },
 });
 
--- backend/models/services.js
+++ backend/models/services.js
@@ -10,6 +10,10 @@ const ServiceSchema = new mongoose.Schema(
       type: String,
       required: [true, "La descrizione del servizio deve essere fornita"],
     },
+    imgName: {
+      type: String,
+      default: "default_service_image.jpg",
+    },
     price: {
       type: Number,
       required: [true, "Il prezzo del servizio deve essere fornito"],
@@ -22,28 +26,6 @@ const ServiceSchema = new mongoose.Schema(
         "Dove viene offerto questo servizio deve essere fornito",
       ],
     },
-    availability: [
-      {
-        date: {
-          type: Date,
-          required: [true, "La data deve essere fornita"],
-        },
-        time: {
-          type: Object,
-          default: {
-            9: true,
-            10: true,
-            11: true,
-            12: true,
-            13: true,
-            14: true,
-            15: true,
-            16: true,
-            17: true,
-          },
-        },
-      },
-    ],
   },
   { timestamps: true }
 );
