546eedd42dd475a6b5a46abe33394d46661d4453
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-22 12:52:28 +0100

    footer styling on small screens fixed

--- game/src/components/Navbar/Footer.vue
+++ game/src/components/Navbar/Footer.vue
@@ -1,6 +1,6 @@
 <script setup></script>
 <template>
-    <footer class="border-t-4 bg-heliotrope-400 border-black text-black mt-3 t font-bold w-full flex flex-col md:flex-row md:justify-between items-center text-sm h-32 md:space-y-0 md:px-3">
+    <footer class="border-t-4 bg-heliotrope-400 border-black text-black mt-3 t font-bold w-full flex flex-col md:flex-row md:justify-between items-center text-sm h-36 lg:h-32 md:space-y-0 md:px-3">
             <p class="order-2 md:order-1 tracking-tight bg-white p-2 rounded-lg"> &copy; AnimalHouse, 2023. </p>
             <div class="order-1 md:order-2 text-center bg-white p-2 rounded-lg mx-2 md:my-0 my-2">
                 <p class="tracking-tight"> Contributors: </p>
