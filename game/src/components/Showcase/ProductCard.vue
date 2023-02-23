bdf72f57982e88b2b02f812e977ea03dac942c86
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-23 14:54:56 +0100

    showcase completed

---
+++ game/src/components/Showcase/ProductCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="flex flex-col justify-between max-w-[300px] bg-pink-400 border-2 border-pink-400 rounded-lg overflow-hidden py-6 shadow-xl">
+      <img class="p-3 overflow-hidden rounded-lg" :src="this.imgUrl" />
+      <div class="p-4 bg-white">
+        <p class="text-black font-bold text-2xl">{{ name }}</p>
+        <p class="mt-2 text-black truncate">
+          {{ description }}
+        </p>
+        <div class="flex item-center justify-between mt-3">
+          <h1 class="text-black font-bold text-xl">€{{ price.toFixed(2) }}</h1>
+          <a
+            :href="this.productLink"
+            class="px-3 py-2 bg-green-400 text-black text-xs font-bold uppercase rounded"
+          >
+            Dettagli
+            </a>
+        </div>
+      </div>
+    </div>
+</template>
+<script>
+export default {
+  props: ["name", "imgName", "description", "_id", "price", "type"],
+  name: "ProductCard",
+  computed: {
+    imgUrl(){
+        return import.meta.env.VITE_BASE_MEDIA_URL + "/" + this.imgName
+    },
+    productLink(){
+        if (this.type === "product")
+          return import.meta.env.VITE_BASE_URL + "/frontoffice/marketplace/" + this._id
+        else
+          return import.meta.env.VITE_BASE_URL + "/frontoffice/services/" + this._id
+    }
+  }
+};
+</script>
--- game/src/components/Showcase/ShowcasePage.vue
+++ game/src/components/Showcase/ShowcasePage.vue
@@ -1,2 +1,65 @@
-<script setup></script>
-<template></template>
+<template>
+<div class="flex flex-col items-center mt-12 gap-y-12" v-show="!this.loading && this.products.length > 0 && this.services.length > 0">
+    <header class="flex flex-col justify-center items-center gap-y-2">
+        <h1
+        class="font-semibold tracking-tighter text-center text-3xl text-black rounded-full bg-green-400 w-fit mx-auto p-3"
+        >
+        Vetrina di AnimalHouse
+        </h1>
+        <a class="underline bg-green-400 p-2" :href="frontofficeUrl"> Clicca qui per andare al portale </a>
+    </header>
+    <h2
+      class="font-semibold tracking-tighter text-center text-2xl text-black rounded-full w-fit mx-auto p-3"
+    >
+      Prodotti in vendita
+    </h2>
+    <div class="grid md:grid-cols-3 grid-cols-1 gap-x-4 px-2 gap-y-4">
+        <ProductCard v-for="product of products" :key="product._id" v-bind="product" type="product" />
+    </div>
+    <h3
+      class="font-semibold tracking-tighter text-center text-2xl text-black rounded-full w-fit mx-auto p-3"
+    >
+      Servizi a disposizione
+    </h3>
+    <div class="grid md:grid-cols-3 grid-cols-1 gap-x-4 px-2 gap-y-4">
+        <ProductCard v-for="service of services" :key="service._id" v-bind="service" :name="service.serviceName" type="service" />
+    </div>
+</div>
+<div class="flex flex-col items-center gap-y-2 mt-12" v-show="this.loading">
+      <PacmanLoader color="#ff2a99" />
+      <small class="font-bold"> Ottenendo servizi e prodotti... </small>
+</div>
+</template>
+<script>
+import { getShowcaseProducts, getShowcaseServices } from '../../common/showcase';
+import PacmanLoader from "vue-spinner/src/PacmanLoader.vue";
+import ProductCard from './ProductCard.vue';
+
+const productsLimit = 3
+const servicesLimit = 3
+
+export default{
+    name: "ShowcasePage",
+    components: {
+        PacmanLoader,
+        ProductCard
+    },
+    async mounted() {
+        this.products = await getShowcaseProducts(productsLimit)
+        this.services = await getShowcaseServices(servicesLimit)
+        this.loading = false
+    },
+    computed: {
+        frontofficeUrl() {
+            return import.meta.env.VITE_BASE_URL + "/frontoffice"
+        }
+    },
+    data: () => {
+        return {
+            products: [],
+            services: [],
+            loading: true
+        };
+    },
+}
+</script>
