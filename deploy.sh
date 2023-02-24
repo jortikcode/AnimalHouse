241ee3a7827a87e29746ae7d67bea97a8a235aa0
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-24 14:43:42 +0100

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+# install backend dependencies
+cd backend
+npm install
+cd ..
+
+# install game dependencies
+cd game
+npm install
+cd ..
+
+# install frontoffice dependencies
+cd frontoffice
+npm install
+cd ..
+
+# build and copy game to backend folder
+cd game
+npm run build
+rm -rf gameBuild
+mv dist gameBuild
+cp -r ./gameBuild ../backend
+cd ..
+
+# build and copy frontoffice to backend folder
+cd frontoffice
+npm run build
+rm -rf frontofficeBuild
+mv build frontofficeBuild
+cp -r ./frontofficeBuild ../backend
+cd ..
+
+# copy files to department server
+cd backend
+rsync -av * .env.development .env.production node_modules juan.jaramillosaa@lucia.cs.unibo.it:/home/web/site212222/html/AnimalHouse/backend
+cd ../back-office
+rsync -av * juan.jaramillosaa@lucia.cs.unibo.it:/home/web/site212222/html/AnimalHouse/back-office
