f65c2cf9270cb2a38edd9b43c770c373b2006027
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-27 19:11:32 +0100

    error modal fixed

--- frontoffice/src/common/ErrorModal.js
+++ frontoffice/src/common/ErrorModal.js
@@ -6,8 +6,8 @@ const ErrorModal = ({ msg, clearErrorFunction }) => {
       } flex justify-center flex-col items-center inset-0 bg-gray-800 bg-opacity-50 overflow-y-auto h-full w-full`}
       id="my-modal"
     >
-      <div className="bg-white p-6 rounded-lg border-4 border-black w-64 flex items-center flex-col">
-        <p className="font-bold text-red-600">{msg} </p>
+      <div className="bg-white p-6 rounded-lg border-4 border-black md:w-96 w-64 max-w-full flex items-center flex-col">
+        <p className="font-bold text-red-600 break-words">{msg} </p>
         <button
           className="bg-yellow-400 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded mt-6"
           onClick={(e) => {
