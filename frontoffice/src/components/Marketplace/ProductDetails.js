1b8bd319952a2ad692f8b9ce800392f6b54551c6
Juan Guillermo Jaramillo Saa <jjuanjaramillo01@gmail.com>
2023-02-27 19:18:00 +0100

    subcategory join

--- frontoffice/src/components/Marketplace/ProductDetails.js
+++ frontoffice/src/components/Marketplace/ProductDetails.js
@@ -75,7 +75,7 @@ const ProductDetails = () => {
 
             <div className="border-t border-gray-200 pt-4">
               <dt className="font-medium text-gray-900">Sottocategoria</dt>
-              <dd className="mt-2 text-sm text-gray-500">{subcategory}</dd>
+              <dd className="mt-2 text-sm text-gray-500">{subcategory.join(", ")}</dd>
             </div>
 
             <div className="border-t border-gray-200 pt-4">
